b6778cf6fbb844b1c726d9a3fe365a6948344a4c
miss-coder10 <dara.dc10@gmail.com>
2016-12-08 17:32:32 +0000

    initial commit

---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+
+  scope :api do
+    resources :users, except: [:create]
+    post 'register', to: 'auth#register'
+    post 'login', to: 'auth#login'
+  end
+
+  resources :users
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
